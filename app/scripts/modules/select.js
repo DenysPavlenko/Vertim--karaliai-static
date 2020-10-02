170302a5901c6b32c55100c878a0a6ae75b046fb
Denys Pavlenko <de.pavlenko@gmail.com>
2020-10-02 11:09:48 +0300

    selects were refactored. Select inner search was implemented

--- app/scripts/modules/select.js
+++ app/scripts/modules/select.js
@@ -4,14 +4,16 @@ export default function () {
   $selects.each(function () {
     var $select = $(this);
     var $selectLabel = $select.find('.js-select-label');
+    var selectedOption = $selectLabel.attr('data-selected');
     var $scrollbar = $select.find('[data-select-simplebar]');
-    var $optionsWrap = $select.find('.js-select-options');
+    var $optionsWrap = $select.find('.js-select-wrap');
     var $options = $select.find('.js-select-option');
     var $input = $select.find('.js-select-input');
+    var $search = $select.find('.js-select-search');
     var optionsWrapWidth;
-    $select.on('click', function () {
-      $select.toggleClass('is-expanded');
-      $optionsWrap.fadeToggle(100);
+    // Toggle select on label click
+    $selectLabel.on('click', function () {
+      toggleSelect();
       optionsWrapWidth = optionsWrapWidth ? optionsWrapWidth : $optionsWrap.width();
       $optionsWrap.css('width', optionsWrapWidth);
       if ($scrollbar.length > 0) {
@@ -20,24 +22,69 @@ export default function () {
         }, 0);
       }
     });
+    // Select option click logic
     $options.each(function () {
       var $option = $(this);
+      var optionData = $option.attr('data-option');
+      if (selectedOption.toLowerCase() === optionData.toLowerCase()) {
+        $option.addClass('is-selected');
+      }
       $option.on('click', function (e) {
         e.stopPropagation();
-        var $optionData = $option.attr('data-option');
-        var $optionContent = $option.html();
-        $selectLabel.attr('data-selected', $optionData);
-        $selectLabel.html($optionContent);
-        $input.attr('value', $optionData);
-        $select.removeClass('is-expanded');
-        $optionsWrap.fadeOut(100);
+        var optionContent = $option.html();
+        $selectLabel.attr('data-selected', optionData);
+        $selectLabel.html(optionContent);
+        $input.attr('value', optionData);
+        $options.removeClass('is-selected');
+        $option.addClass('is-selected');
+        collapseSelect();
       });
     });
+    // Select search
+    $search.on('input', function (e) {
+      var value = e.target.value.toLowerCase();
+      $options.each(function () {
+        var $option = $(this);
+        var $optionText = $option.find('span').text().toLowerCase();
+        if ($optionText.indexOf(value) == -1) {
+          $option.hide();
+        } else {
+          $option.show();
+        }
+      });
+    });
+    // Collapse select on document click
     $(document).on('click', function (e) {
       if ($(e.target).parents('.js-select')[0] !== $select[0]) {
-        $select.removeClass('is-expanded');
-        $optionsWrap.fadeOut(100);
+        collapseSelect();
       }
     });
+
+    // Toggle select
+    function toggleSelect() {
+      $select.toggleClass('is-expanded');
+      $optionsWrap.fadeToggle(100, function () {
+        if ($search) {
+          $search.val('');
+        }
+        $options.each(function () {
+          var $option = $(this)
+          $option.css('display') === 'none' && $option.show();
+        });
+      });
+    }
+    // Collapse select
+    function collapseSelect() {
+      $select.removeClass('is-expanded');
+      $optionsWrap.fadeOut(100, function () {
+        if ($search) {
+          $search.val('');
+        }
+        $options.each(function () {
+          var $option = $(this)
+          $option.css('display') === 'none' && $option.show();
+        });
+      });
+    }
   });
 }
