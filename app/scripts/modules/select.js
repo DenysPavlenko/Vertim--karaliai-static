df5012ba1486ddf1bc4b35a4f8365e717e9a6d03
Denys Pavlenko <de.pavlenko@gmail.com>
2020-10-05 17:29:46 +0300

    arrows control was added to select

--- app/scripts/modules/select.js
+++ app/scripts/modules/select.js
@@ -2,46 +2,70 @@ export default function () {
   var $selects = $('.js-select');
 
   $selects.each(function () {
+    var isOpened = false;
     var $select = $(this);
     var $selectLabel = $select.find('.js-select-label');
-    var selectedOption = $selectLabel.attr('data-selected');
-    var $scrollbar = $select.find('[data-select-simplebar]');
-    var $optionsWrap = $select.find('.js-select-wrap');
+    var $selectDropdown = $select.find('.js-select-dropdown');
+    var $selectScrollbar = $select.find('[data-select-simplebar]');
+    var $optionsContainer = $select.find('.js-select-options');
     var $options = $select.find('.js-select-option');
     var $input = $select.find('.js-select-input');
     var $search = $select.find('.js-select-search');
-    var optionsWrapWidth;
+    var selectedOption = $selectLabel.attr('data-selected') && $selectLabel.attr('data-selected').toLowerCase();
+    var selectDropdownWidth;
+    // Set a default value to the input if there is a default option
+    if (selectedOption) {
+      $input.val(selectedOption);
+    }
     // Toggle select on label click
     $selectLabel.on('click', function () {
       toggleSelect();
-      optionsWrapWidth = optionsWrapWidth ? optionsWrapWidth : $optionsWrap.width();
-      $optionsWrap.css('width', optionsWrapWidth);
-      if ($scrollbar.length > 0) {
+      // Set fixed width to select dropdown
+      selectDropdownWidth = selectDropdownWidth ? selectDropdownWidth : $selectDropdown.width();
+      $selectDropdown.css('width', selectDropdownWidth);
+      // Initialize SimpleBar scroll
+      if ($selectScrollbar.length > 0) {
         setTimeout(function () {
-          new SimpleBar($scrollbar[0]);
+          new SimpleBar($selectScrollbar[0]);
         }, 0);
       }
     });
     // Select option click logic
     $options.each(function () {
       var $option = $(this);
-      var optionData = $option.attr('data-option');
-      if (selectedOption.toLowerCase() === optionData.toLowerCase()) {
+      // Set is-selected class if the option is selected by default
+      var optionData = $option.attr('data-option').toLowerCase();
+      if (selectedOption === optionData) {
         $option.addClass('is-selected');
       }
+      // Set option by the option click
       $option.on('click', function (e) {
         e.stopPropagation();
-        var optionContent = $option.html();
-        $selectLabel.attr('data-selected', optionData);
-        $selectLabel.html(optionContent);
-        $input.attr('value', optionData);
-        $options.removeClass('is-selected');
-        addWave($option, e);
+        setOption($option);
+        addWaveAnimation($option, e);
         collapseSelect(function () {
           $option.addClass('is-selected');
         });
       });
     });
+    // Options arrow navigation
+    $select.on('keydown', function (e) {
+      if (!isOpened) { return; }
+      if (e.keyCode === 40) {
+        e.preventDefault();
+        optionsNavigation('next');
+      }
+      if (e.keyCode === 38) {
+        e.preventDefault();
+        optionsNavigation('prev');
+      }
+      if (e.keyCode === 13) {
+        e.preventDefault();
+        var $focusedOption = $optionsContainer.find('.js-select-option.is-focused');
+        setOption($focusedOption);
+        toggleSelect();
+      }
+    })
     // Select search
     $search.on('input', function (e) {
       var value = e.target.value.toLowerCase();
@@ -62,30 +86,46 @@ export default function () {
       }
     });
 
+
     // Toggle select
     function toggleSelect() {
+      isOpened = !isOpened;
       $select.toggleClass('is-expanded');
-      $optionsWrap.fadeToggle(100, function () {
+      $selectDropdown.fadeToggle(100, function () {
+        // Clear search bar
         if ($search) {
           $search.val('');
         }
         $options.each(function () {
           var $option = $(this);
-          removeWave($option);
+          stopWaveAnimation($option);
           $option.css('display') === 'none' && $option.show();
         });
       });
     }
+
+    // Set option
+    function setOption(option) {
+      var optionHtml = option.html();
+      var optionData = option.attr('data-option');
+      $selectLabel.attr('data-selected', optionData);
+      $selectLabel.html(optionHtml);
+      $input.attr('value', optionData);
+      $options.removeClass('is-selected');
+    }
+
     // Collapse select
     function collapseSelect(callback) {
+      isOpened = false;
       $select.removeClass('is-expanded');
-      $optionsWrap.fadeOut(300, function () {
+      $selectDropdown.fadeOut(300, function () {
         if ($search) {
           $search.val('');
         }
         $options.each(function () {
           var $option = $(this);
-          removeWave($option);
+          stopWaveAnimation($option);
+          $option.removeClass('is-focused');
           $option.css('display') === 'none' && $option.show();
         });
         if (callback) {
@@ -93,8 +133,27 @@ export default function () {
         }
       });
     }
+
+    // Navigat through options by arrows. Direction can be next() or prev()
+    function optionsNavigation(direction) {
+      var $selectedOption = $optionsContainer.find('.js-select-option.is-selected');
+      var $focusedOption = $optionsContainer.find('.js-select-option.is-focused');
+      if ($selectedOption.length && !$focusedOption.length) {
+        if ($selectedOption[direction]().length > 0) {
+          $selectedOption[direction]().addClass('is-focused');
+        }
+      } else if ($focusedOption.length) {
+        if ($focusedOption[direction]().length > 0) {
+          $focusedOption.removeClass('is-focused');
+          $focusedOption[direction]().addClass('is-focused');
+        }
+      } else if (!$selectedOption.length && !$focusedOption.length) {
+        $options.eq(0).addClass('is-focused');
+      }
+    }
+
     // Wave animation
-    function addWave(elem, e) {
+    function addWaveAnimation(elem, e) {
       var wave, d, x, y;
       if (!elem.find(".select__option-wave").length) {
         elem.prepend("<span class='select__option-wave'></span>")
@@ -109,8 +168,9 @@ export default function () {
         wave.remove();
       })
     }
+
     // Force remove wave animation
-    function removeWave(elem) {
+    function stopWaveAnimation(elem) {
       if (elem.find(".select__option-wave").length) {
         elem.find(".select__option-wave").remove();
       }
