2b008a757149e83a82db42fbdd7c8a018e2913b0
Denys Pavlenko <de.pavlenko@gmail.com>
2020-10-07 15:52:01 +0300

    upload document reactions were addde

--- app/scripts/modules/upload-documents-box.js
+++ app/scripts/modules/upload-documents-box.js
@@ -5,10 +5,13 @@ export default function () {
   if (!$boxes.length) { return; }
 
   $boxes.each(function () {
-    var box = this;
-    // var boxLabel
-    new Dropzone(box, {
-      previewTemplate: box.querySelector('.upload-documents-box__tpl').innerHTML,
+    var $box = $(this);
+    var $boxParent = $box.parents('.upload-documents');
+    var $downloadButton = $boxParent.find('.upload-documents__header-button button');
+    var $emptyText = $boxParent.find('.upload-documents__header-empty');
+
+    new Dropzone($box[0], {
+      previewTemplate: $box.find('.upload-documents-box__tpl').html(),
       url: "/",
       uploadprogress: function (file, progress) {
         var $document = $(file.previewElement);
@@ -19,10 +22,19 @@ export default function () {
       },
       init: function () {
         this.on('addedfile', function () {
-          var $box = $(this.element);
           var element = $box.find('.document-list-item');
           var wrapper = $box.find('.simplebar-content');
           wrapper.append(element);
+          if (this.files.length > 0) {
+            $downloadButton.removeClass('button--disabled');
+            $emptyText.hide();
+          }
+        });
+        this.on('removedfile', function (e) {
+          if (this.files.length <= 0) {
+            $downloadButton.addClass('button--disabled');
+            $emptyText.show();
+          }
         });
       },
       dragenter: function (props) {
